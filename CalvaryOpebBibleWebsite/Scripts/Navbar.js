fea1c631aafb76843a2179a4a120e17b52c12fee
JoelGraves <Jgraves723@gmail.com>
2015-04-10 12:23:32 -0700

    Sticky navbar implemented
    Jquery is hard coded. changing the header will likely mess with the
    navbar.

---
+++ CalvaryOpebBibleWebsite/Scripts/Navbar.js
@@ -0,0 +1,18 @@
+﻿
+var mn = $(".main-nav"); //alert("mn=" + mn);
+var mns = "main-nav-scrolled"; //alert("mns=" + mns);
+var hdr = $('header').height(); //alert("hdr=" + hdr);
+
+
+$(window).scroll(function () {
+    //alert("inside function");
+    if ($(this).scrollTop() > 390) {
+        //alert("inside if");
+        $(".main-nav").addClass("main-nav-scrolled");
+    } else {
+        //alert("inside else");
+        $(".main-nav").removeClass("main-nav-scrolled");
+    }
+});
+
+
