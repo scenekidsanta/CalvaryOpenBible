9bd98bca3ba181bbe93793313c00280ecf7ba0f9
Jordan Poet <Jpoet1291@gmail.com>
2015-04-11 21:00:31 -0700

    Worked on scaffolding views
    Scaffolded both belief, and pastors pages, and added navigation to them
    from the about page

--- CalvaryOpebBibleWebsite/Models/Event.cs
+++ CalvaryOpebBibleWebsite/Models/Event.cs
@@ -29,6 +29,7 @@ namespace CalvaryOpebBibleWebsite.Models
 
         [Required]
         public string EventLocation { get; set; }
+        public DateTime EventTime { get; set; }
 
         // Thought this might be good to have, but could definitely get rid of
         public string EventCoordinator { get; set; }
